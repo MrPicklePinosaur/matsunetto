adc824b05e2f5fa1b2e9b5443679f785a02bb729
Daniel Liu <mr.picklepinosaur@gmail.com>
2022-08-23 00:54:13 -0400

    periodically push metrics

--- mnet_client/src/main.rs
+++ mnet_client/src/main.rs
@@ -4,17 +4,25 @@ pub mod api;
 pub mod config;
 pub mod metrics;
 
+use std::thread;
+
 use anyhow::Result;
 use api::push_metrics;
 use metrics::*;
 use mnet_types::Metrics;
+use systemstat::Duration;
 
 fn main() -> Result<()> {
-    let metrics = Metrics {
-        battery: battery()?,
-        memory: memory()?,
-        uptime: uptime()?,
-    };
-    push_metrics(&metrics);
+    loop {
+        let metrics = Metrics {
+            battery: battery()?,
+            memory: memory()?,
+            uptime: uptime()?,
+        };
+        push_metrics(&metrics);
+        thread::sleep(Duration::from_secs(60));
+        println!("data pushed");
+    }
+
     Ok(())
 }
